7e5ee7badb5053372b9c21d1fcd993cc94c9fd9b
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-04-19 13:04:52 +0200

    Try 1,000,000

--- vectorlink/src/batch.rs
+++ vectorlink/src/batch.rs
@@ -504,7 +504,7 @@ mod tests {
     // where do a put temp files for testing?
     #[test]
     fn test_comparator16_pq() {
-        let vectors: Vec<Embedding> = (0..10_000)
+        let vectors: Vec<Embedding> = (0..1_000_000)
             .map(|i| {
                 let prng = StdRng::seed_from_u64(42_u64 + i as u64);
                 let range = Uniform::from(-1.0..1.0);
