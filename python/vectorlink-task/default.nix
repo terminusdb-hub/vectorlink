fd82839175888c80197d210fd868731520c1cb30
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-05-12 23:25:19 +0200

    correct name of task python package

--- python/vectorlink-task/default.nix
+++ python/vectorlink-task/default.nix
@@ -2,7 +2,7 @@
 with pkgs;
 with pkgs.python311Packages;
 buildPythonPackage rec {
-  name = "vectorlink_vectorize";
+  name = "vectorlink_task";
   src = ./.;
   format = "pyproject";
   propagatedBuildInputs = [
