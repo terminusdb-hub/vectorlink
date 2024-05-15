920118e7c456bd77ece4ec506bd5912ec5e55b28
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-05-15 09:47:50 +0200

    package concat tool

--- flake.nix
+++ flake.nix
@@ -40,6 +40,7 @@
           vectorlink = pkgs.callPackage ./vectorlink {};
           vectorlink-worker = pkgs.callPackage ./vectorlink-worker {};
           line-index = pkgs.callPackage ./line-index {};
+          snowflake-concat = pkgs.callPackage ./snowflake-concat {};
           vectorlink-infra = pkgs.callPackage ./vectorlink-infra {};
           vectorlink-task-monitor = unstablepkgs.callPackage python/vectorlink-task {
             config = { allowUnfree = true;
