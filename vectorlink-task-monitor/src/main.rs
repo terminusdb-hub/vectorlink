b7b7ec4049b6fb33bddbe23c2fd52bce121faa59
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-05-27 00:44:08 +0200

    implement resuming of unclaimed tasks

--- vectorlink-task-monitor/src/main.rs
+++ vectorlink-task-monitor/src/main.rs
@@ -8,7 +8,8 @@ use futures::TryStreamExt;
 use serde_json::json;
 use vectorlink_task::{
     key::{
-        claim_key, get_increment_key, key_after_prefix, queue_key, task_key_task_id, TASKS_PREFIX,
+        claim_key, claim_key_task_id, get_increment_key, interrupt_key, key_after_prefix,
+        queue_key, task_key, task_key_task_id, CLAIMS_PREFIX, TASKS_PREFIX,
     },
     task::{TaskData, TaskStatus},
 };
@@ -27,8 +28,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     // to start, we have to process any tasks already on the queue.
     let revision = process_existing_tasks(&mut client).await?;
 
-    // then we can start our watch
-    process_new_tasks(&mut client, revision + 1).await?;
+    // then we can start our watches
+    let mut task_client = client.clone();
+    tokio::select! {
+        _ = process_new_tasks(&mut task_client, revision + 1) => {},
+        _ = process_new_orphans(&mut client, revision+1) => {}
+    }
 
     Ok(())
 }
@@ -100,28 +105,86 @@ async fn process_new_tasks(client: &mut Client, revision: i64) -> Result<(), etc
         }
     }
 
-    eprintln!("leaving the process loop");
+    eprintln!("leaving the task process loop");
+    Ok(())
+}
+
+async fn process_new_orphans(client: &mut Client, revision: i64) -> Result<(), etcd_client::Error> {
+    eprintln!("start watching for orphaned tasks");
+    let (_watcher, mut watch_stream) = client
+        .watch(
+            CLAIMS_PREFIX,
+            Some(
+                WatchOptions::new()
+                    .with_prefix()
+                    .with_start_revision(revision)
+                    .with_filters([WatchFilterType::NoPut])
+                    .with_fragment(),
+            ),
+        )
+        .await?;
+
+    while let Some(response) = watch_stream.try_next().await? {
+        if response.canceled() {
+            break;
+        }
+        let current_revision = response.header().expect("no header").revision();
+        for event in response.events() {
+            let kv = event.kv();
+            if kv.is_none() {
+                // weird, but whatever
+                continue;
+            }
+            let kv = kv.unwrap();
+
+            let task_id = claim_key_task_id(kv.key());
+            let task_key = task_key(task_id);
+
+            let task_kv = client
+                .get(
+                    task_key,
+                    Some(GetOptions::new().with_revision(current_revision)),
+                )
+                .await?
+                .take_kvs()
+                .into_iter()
+                .next()
+                .unwrap();
+
+            try_enqueue_task(client, &task_kv).await?;
+        }
+    }
+
+    eprintln!("leaving the orphan process loop");
     Ok(())
 }
 
 async fn try_enqueue_task(client: &mut Client, kv: &KeyValue) -> Result<(), etcd_client::Error> {
-    match serde_json::from_reader::<_, TaskData>(kv.value()).map(|v| v.status) {
-        Ok(TaskStatus::Pending) | Ok(TaskStatus::Resuming) | Ok(TaskStatus::Running) => {
-            // enqueue - inner enqueue logic ensures that already running tasks aren't re-queued.
-            enqueue_task(client, kv.key()).await?;
-        }
-        Ok(_) => {
-            // nothing to do for this task
+    match serde_json::from_reader::<_, TaskData>(kv.value()) {
+        Ok(parsed) => {
+            match parsed.status {
+                TaskStatus::Pending | TaskStatus::Resuming => {
+                    // enqueue - inner enqueue logic ensures that already running tasks aren't re-queued.
+                    enqueue_task(client, kv.key()).await?;
+                }
+                TaskStatus::Running => {
+                    // resume if no claim
+                    resume_unclaimed(client, kv, parsed).await?;
+                }
+                _ => {
+                    // nothing to do for this task
+                }
+            }
         }
         Err(e) => {
             eprintln!(
                 "unparsable task {:?}: {e}",
                 kv.key_str().unwrap_or("with non-string key")
             );
-            // yikes, a task on the queue is not good. We should immediately error the task, if it is still there in that shape.
+            // yikes, a task on the queue is not good. We should immediately error the task, if it is still at the same version.
             let _ = client.txn(Txn::new()
                        .when([
-                           Compare::value(kv.key(), CompareOp::Equal, kv.value())
+                           Compare::version(kv.key(), CompareOp::Equal, kv.version())
                        ])
                        .and_then([
                            TxnOp::put(kv.key(), serde_json::to_vec(&json!({"status":"error","error":format!("unparsable task: {e}"), "original": String::from_utf8_lossy(kv.value())})).unwrap(), None)
@@ -151,6 +214,40 @@ async fn enqueue_task(client: &mut Client, task_key: &[u8]) -> Result<(), etcd_c
 
     if result.succeeded() {
         eprintln!("enqueue {}", String::from_utf8_lossy(task_key));
+    }
+
+    Ok(())
+}
+
+async fn resume_unclaimed(
+    client: &mut Client,
+    kv: &KeyValue,
+    mut task_data: TaskData,
+) -> Result<(), etcd_client::Error> {
+    task_data.status = TaskStatus::Resuming;
+
+    let task_id = task_key_task_id(kv.key());
+    let interrupt_key = interrupt_key(task_id);
+    let claim_key = claim_key(task_id);
+
+    let serialized = serde_json::to_vec(&task_data).expect("serialization of task failed");
+
+    let result = client
+        .txn(
+            Txn::new()
+                .when([
+                    Compare::version(kv.key(), CompareOp::Equal, kv.version()),
+                    Compare::version(claim_key, CompareOp::Equal, 0),
+                ])
+                .and_then([
+                    TxnOp::put(kv.key(), serialized, None),
+                    TxnOp::delete(interrupt_key, None),
+                ]),
+        )
+        .await?;
+
+    if result.succeeded() {
+        eprintln!("resume {}", String::from_utf8_lossy(kv.key()));
     }
 
     Ok(())
