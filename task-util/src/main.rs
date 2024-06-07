663c59f280be93081370aa370030b327869ce5f3
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-06-07 12:17:05 +0200

    resume from prefix

--- task-util/src/main.rs
+++ task-util/src/main.rs
@@ -14,16 +14,20 @@ struct Command {
 #[derive(Debug, Clone, Subcommand)]
 enum Subcommands {
     Resume { task_id: String },
+    ResumeAll { task_prefix: String },
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Command::parse();
-    let client = Client::connect(args.etcd, None).await?;
+    let mut client = Client::connect(args.etcd, None).await?;
 
     match args.command {
         Subcommands::Resume { task_id } => {
-            resume::resume(client, task_id).await?;
+            resume::resume(&mut client, &task_id).await?;
+        }
+        Subcommands::ResumeAll { task_prefix } => {
+            resume::resume_all(&mut client, &task_prefix).await?;
         }
     }
 
--- task-util/src/resume.rs
+++ task-util/src/resume.rs
@@ -1,11 +1,12 @@
-use std::process;
-
-use etcd_client::{Client, Compare, Txn, TxnOp};
-use vectorlink_task::task::{TaskData, TaskStatus};
+use etcd_client::{Client, Compare, GetOptions, Txn, TxnOp};
+use vectorlink_task::{
+    key::task_key_task_id,
+    task::{TaskData, TaskStatus},
+};
 
 pub async fn resume(
-    mut client: Client,
-    task_id: String,
+    client: &mut Client,
+    task_id: &str,
 ) -> Result<bool, Box<dyn std::error::Error>> {
     let key = format!("/services/tasks/{task_id}").into_bytes();
     let vals = client.get(key.clone(), None).await?;
@@ -19,10 +20,7 @@ pub async fn resume(
     let mut task_data: TaskData =
         serde_json::from_reader(kv.value()).expect("task was not valid task data");
     if task_data.status != TaskStatus::Error {
-        eprintln!(
-            "task status is {:?}, so no resume happened.",
-            task_data.status
-        );
+        eprintln!("skipped task {task_id} ({:?})", task_data.status);
         return Ok(false);
     }
 
@@ -45,10 +43,27 @@ pub async fn resume(
         .await?;
 
     if result.succeeded() {
-        eprintln!("resumed task {task_id}.");
+        eprintln!("resumed task {task_id}");
         Ok(true)
     } else {
         eprintln!("task {task_id} changed while processing.");
         Ok(false)
     }
+}
+
+pub async fn resume_all(
+    client: &mut Client,
+    task_prefix: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let prefix_key = format!("/services/tasks/{task_prefix}").into_bytes();
+    let result = client
+        .get(prefix_key.clone(), Some(GetOptions::new().with_prefix()))
+        .await?;
+
+    for kv in result.kvs() {
+        let task_id = std::str::from_utf8(task_key_task_id(kv.key())).unwrap();
+        let _ = resume(client, task_id).await?;
+    }
+
+    Ok(())
 }
