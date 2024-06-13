1612592c7b6bf24dcdf9ce50760bfe12f88e47b9
jmacak <jmacak@decisionlinks.com>
2024-06-13 16:54:56 -0400

    Moved prometheus testing to function

--- vectorlink-worker/src/main.rs
+++ vectorlink-worker/src/main.rs
@@ -2,7 +2,10 @@ mod handler;
 
 use clap::Parser;
 use vectorlink_task::{queue::Queue, task::TaskHandler};
-use prometheus_exporter::{self, prometheus::register_counter};
+use prometheus_exporter::{
+    self, 
+    prometheus::{register_counter, register_gauge, TextEncoder, gather},
+};
 
 use crate::handler::VectorlinkTaskHandler;
 
@@ -20,19 +23,32 @@ fn generate_identity() -> String {
     "worker".to_string()
 }
 
-fn init_prometheus() {
+fn test_prometheus() {
+    // register metrics to default registry
     let counter = register_counter!("test_counter", "Number of tasks processed").unwrap();
     let gauge = register_gauge!("test_gauge", "Value of metric").unwrap();
+
+    //increment counters
     counter.inc_by(1.0);
-    gauge.set(1.0);
-    (counter, gauge)
+    gauge.set(1.0); 
+    counter.inc_by(1.0);
+    gauge.inc_by(-6.0);
+
+    // gather default registry
+    let metric_families = gather();
+
+    // encode MetricFamilys
+    let mut buffer = vec![];
+    let encoder = TextEncoder::new();
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    // print to stdout
+    println!("{}", String::from_utf8(buffer).unwrap());
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (counter, gauge) = init_prometheus();
-    counter.inc_by(1.0);
-    gauge.inc_by(-6.0);
+    let (counter, gauge) = test_prometheus();
     let args = Command::parse();
     let mut queue = Queue::connect(
         args.etcd,
