530e3d37044a2e1de7f2eca10e0bf60844e879ae
jmacak <jmacak@decisionlinks.com>
2024-06-14 11:30:01 -0400

    added metrics to code

--- vectorlink-worker/src/main.rs
+++ vectorlink-worker/src/main.rs
@@ -1,13 +1,14 @@
-mod handler;
+// mod handler;
 
 use clap::Parser;
-use vectorlink_task::{queue::Queue, task::TaskHandler};
+use prometheus::core::{AtomicF64, GenericCounter};
+// use vectorlink_task::{queue::Queue, task::TaskHandler};
 use prometheus_exporter::{
     self, 
     prometheus::{register_counter, register_gauge, TextEncoder, gather},
 };
 
-use crate::handler::VectorlinkTaskHandler;
+// use crate::handler::VectorlinkTaskHandler;
 
 #[derive(Parser, Debug)]
 struct Command {
@@ -23,44 +24,51 @@ fn generate_identity() -> String {
     "worker".to_string()
 }
 
-fn test_prometheus() {
-    // register metrics to default registry
-    let counter = register_counter!("test_counter", "Number of tasks processed").unwrap();
-    let gauge = register_gauge!("test_gauge", "Value of metric").unwrap();
+fn start_prometheus_exporter() -> () {
+    let binding = "127.0.0.1:9184".parse().unwrap();
+    prometheus_exporter::start(binding).unwrap();
+}
 
-    //increment counters
-    counter.inc_by(1.0);
-    gauge.set(1.0); 
-    counter.inc_by(1.0);
-    gauge.inc_by(-6.0);
+fn register_metrics() -> (GenericCounter<AtomicF64>, GenericCounter<AtomicF64>, GenericCounter<AtomicF64>) {
+    let worker_started_counter = register_counter!("worker_started_counter", "Number of workers started").unwrap();
+    let successful_connection_counter = register_counter!("successful_connection_counter", "Number of successful connections to etcd").unwrap();
+    let successful_task_counter = register_counter!("successful_task_counter", "Number of successful tasks processed").unwrap();
 
-    // gather default registry
-    let metric_families = gather();
+    (worker_started_counter, successful_connection_counter, successful_task_counter)
+}
 
-    // encode MetricFamilys
-    let mut buffer = vec![];
-    let encoder = TextEncoder::new();
-    encoder.encode(&metric_families, &mut buffer).unwrap();
-
-    // print to stdout
-    println!("{}", String::from_utf8(buffer).unwrap());
+fn wait() -> (){
+    use std::io::{self, Write};
+    println!("Press enter to continue...");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let binding = "127.0.0.1:9184".parse().unwrap();
-    prometheus_exporter::start(binding).unwrap();
-    
-    let (counter, gauge) = test_prometheus();
-    let args = Command::parse();
-    let mut queue = Queue::connect(
-        args.etcd,
-        None,
-        args.service,
-        args.identity.unwrap_or_else(generate_identity),
-    )
-    .await?;
-    VectorlinkTaskHandler::process_queue(&mut queue).await?;
 
+    start_prometheus_exporter();
+    
+    let (worker_started_counter, successful_connection_counter, successful_task_counter) = register_metrics();
+    let metric_families = gather();
+    let encoder = TextEncoder::new();
+    encoder.encode_to_string(&metric_families).unwrap(); // not sure yet if this is necessary
+
+    worker_started_counter.inc();
+
+    // let args = Command::parse();
+    // let mut queue = Queue::connect(
+    //     args.etcd,
+    //     None,
+    //     args.service,
+    //     args.identity.unwrap_or_else(generate_identity),
+    // )
+    // .await?;
+    successful_connection_counter.inc();
+
+    // VectorlinkTaskHandler::process_queue(&mut queue).await?;
+    successful_task_counter.inc();
+    
+    // wait(); // for testing; you can see the metrics at http://localhost:9184/metrics
     unreachable!();
 }
