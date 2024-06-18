c202afa8c58af91c5f7a7bc64d978a79b888814b
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-06-18 12:03:45 +0200

    comment out

--- vectorlink-task/src/panic.rs
+++ vectorlink-task/src/panic.rs
@@ -26,8 +26,8 @@ pub fn set_panic_hook() {
     let old_hook = take_hook();
     set_hook(Box::new(move |info| {
         CURRENT_TASK.with(|t| {
-            //if let Some(t) = t.borrow().as_ref() {
-            if false {
+            /*
+            if let Some(t) = t.borrow().as_ref() {
                 let msg = if let Some(p) = info.payload().downcast_ref::<&str>() {
                     p
                 } else if let Some(p) = info.payload().downcast_ref::<String>() {
@@ -42,8 +42,9 @@ pub fn set_panic_hook() {
                 let mut error_map = LAST_ERRORS.lock().expect("could not retrieve error map!");
                 error_map.insert(t.clone(), error);
             } else {
-                old_hook(info);
-            }
+            */
+            old_hook(info);
+            //}
         })
     }));
 }
