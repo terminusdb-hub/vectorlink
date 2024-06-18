b0db47682b2a22e6c927c06ed529e4d4263c8524
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-06-18 15:30:30 +0200

    Adding code

--- vectorlink-quantizer/src/main.rs
+++ vectorlink-quantizer/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+mod handler;
+
+use clap::Parser;
+use vectorlink_task::{queue::Queue, task::TaskHandler};
+
+use crate::handler::QuantizationHandler;
+
+#[derive(Parser, Debug)]
+struct Command {
+    #[arg(short, long, default_value = "Vec::new()")]
+    etcd: Vec<String>,
+    #[arg(short, long, default_value = "vectorlink")]
+    service: String,
+    #[arg(short, long)]
+    identity: Option<String>,
+}
+
+fn generate_identity() -> String {
+    "quantizer".to_string()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Command::parse();
+    let mut queue = Queue::connect(
+        args.etcd,
+        None,
+        args.service,
+        args.identity.unwrap_or_else(generate_identity),
+    )
+    .await?;
+    QuantizationHandler::process_queue(&mut queue).await?;
+
+    unreachable!();
 }
