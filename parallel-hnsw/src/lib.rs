4c1b94c36f436656fffcab213286f749e9e6e72c
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-06-19 11:29:09 +0200

    Do not duplicate layer optimisations that have already been done

--- parallel-hnsw/src/lib.rs
+++ parallel-hnsw/src/lib.rs
@@ -1649,7 +1649,9 @@ impl<C: Comparator + 'static> Hnsw<C> {
             while current_layer_from_top <= layer_from_top && bailout != 0 {
                 let layer_count = self.layer_count();
                 eprintln!("improve_index_at is going to call improve_neighbors_upto");
-                recall = self.improve_neighbors_upto(current_layer_from_top + 1, op, None);
+                self.improve_neighborhoods_at_layer(current_layer_from_top + 1, op.search);
+
+                recall = self.stochastic_recall_at(current_layer_from_top, op);
                 statistics.recall = Some(recall);
                 progress
                     .layer_statistics(layer_from_top, statistics)
