d74d9e1efdf9d6e8c6582f8fb4481a47d784615d
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-06-25 09:20:25 +0200

    properly process extra args

--- nix/modules/parseQueueOptions.nix
+++ nix/modules/parseQueueOptions.nix
@@ -3,11 +3,12 @@ worker-name:
 { enable, description, bin, etcd, service-name, identity-method, extra-args, user, ... }:
 let service-arg = if service-name != null then "--service ${service-name}"  else "";
     etcd-arg = lib.concatStringsSep "," etcd;
+    extra-args' = lib.concatStringsSep " " extra-args;
     user' = if user != null then user else worker-name;
 in
 {
   inherit enable;
   inherit description;
   user = user';
-  cmd = "${bin} ${service-arg} ${etcd-arg} ${extra-args}";
+  cmd = "${bin} ${service-arg} ${etcd-arg} ${extra-args'}";
 }
