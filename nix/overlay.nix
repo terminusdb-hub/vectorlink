e8ff151111374339bb529585b16fd51237563c22
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-06-25 06:13:21 +0200

    remove build-support from packages in overlay for packages that don't need it

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -9,8 +9,8 @@ let vectorlink-build-support = final.callPackage ./build {}; in
     vectorlink-worker = final.callPackage ../vectorlink-worker {inherit vectorlink-build-support;};
     task-util = final.callPackage ../task-util {inherit vectorlink-build-support;};
     line-index = final.callPackage ../line-index {inherit vectorlink-build-support;};
-    vectorlink-vectorize = final.callPackage ../python/vectorlink-vectorize {inherit vectorlink-build-support;};
-    create-vectorize-tasks = final.callPackage ../python/create-vectorize-task {inherit vectorlink-build-support;};
-    read-line-from-index = final.callPackage ../python/read-line-from-index {inherit vectorlink-build-support;};
+    vectorlink-vectorize = final.callPackage ../python/vectorlink-vectorize {};
+    create-vectorize-tasks = final.callPackage ../python/create-vectorize-task {};
+    read-line-from-index = final.callPackage ../python/read-line-from-index {};
   };
 }
