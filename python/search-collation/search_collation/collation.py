9ce6d70053f51a389a7d1a56bb5a999adba41746
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-08-13 15:20:38 +0200

    Read matches

--- python/search-collation/search_collation/collation.py
+++ python/search-collation/search_collation/collation.py
@@ -24,12 +24,12 @@ if __name__ == '__main__':
     with open(input_index, 'rb') as idx:
         idx_buf = idx.read()
         ulongs_in_file = min(10, int(len(idx_buf) / 8))
-        with open(input_file, 'rb') as i:
+        with open(input_file, 'rb') as ifile:
             for i in range(0, ulongs_in_file):
                 start = struct.unpack_from("<L", idx_buf, i * 8)[0]
                 end = struct.unpack_from("<L", idx_buf, (i+1) * 8)[0]
                 size = int( (end - start) / pair_size)
-                queue_buf = i.read(size)
+                queue_buf = ifile.read(size)
                 array = struct.iter_unpack("<Lf", queue_buf)
                 result[i] = []
                 for (vid, _) in list(array):
