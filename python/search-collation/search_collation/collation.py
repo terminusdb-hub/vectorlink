3ed6b08f5f03c4290fb8f5dbfe3d2b1a4f4eb2f2
Gavin Mendel-Gleason <gavin@terminusdb.com>
2024-08-13 15:51:44 +0200

    Push the vectors in

--- python/search-collation/search_collation/collation.py
+++ python/search-collation/search_collation/collation.py
@@ -1,6 +1,7 @@
 import struct
 import argparse
 import sys
+import numpy
 
 def read_offset(istream):
     return struct.unpack("L", istream)
@@ -9,10 +10,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-prefix', help='input match file prefix (before .idx or .match) to interpret', required=True)
     parser.add_argument('-o', '--output-file', help='output file for reordered match', required=True)
-
+    parser.add_argument('-d', '--directory', help='vector files directory', required=True)
     args = parser.parse_args()
 
     # 1. First, load match file.
+
     input_prefix = args.input_prefix
     input_file = f"{input_prefix}.queues"
     input_index = f"{input_prefix}.index"
@@ -35,12 +37,36 @@ if __name__ == '__main__':
                 for (vid, _) in list(array):
                     result[i].append(vid)
 
-    print(result)
     # 2. Prescan vectors for loading from the match file
-    #    * requires offset calculation for match vector
+    #    * requires offset calculation for match vector (but not for 0)
+    ids = []
+    for key in result:
+        ids.append(key)
+        for i in result[key]:
+            ids.append(i)
+
+    ids.sort()
 
     # 3. Preload the vectors into the GPU
+    vector_file_size = 128370618368
+    vector_size = 1536 * 4 # dimension * f32
+    vector_file_count = vector_file_size / vector_size
 
+    file_no = 0
+    f = open(f"{directory}/{file_no}.vecs", 'rb')
+    vecs = numpy.array([])
+    for i in ids:
+        new_file_no = int(i / vector_file_count)
+        if new_file_no != file_no:
+            file_no = new_file_no
+            f.close()
+            f = open(f"{directory}/{file_no}.vecs", 'rb')
+        file_offset = i % file_size * file_no
+        f.seek(file_offset * vector_size)
+        raw_buf = f.read(vector_size)
+        vecs.append(numpy.frombuffer(raw_buf, dtype=float))
+
+    f.close()
     # 4. Perform match calculations and write the output matches as binary structs
     #
     # The match calculation is a dot product of the match vectors and the candidate
